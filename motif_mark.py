cd761ee376cb533d1b11ec78fddb18ee0c24c1ec
Juyoung Lee <jlee26@uoregon.edu>
2022-03-06 06:15:37 -0800

    Working on visualization. Added correct staggering of motifs

--- motif_mark.py
+++ motif_mark.py
@@ -75,33 +75,6 @@ class Motif:
 #motif_findings.extract_motif()
 #motif_findings.y_converter()
 
-class Exon:
-    ''''''
-    def __init__(self, file):
-        '''DOCUMENT'''
-        ## Data ##
-        self.file = file
-    
-    ### Methods ###
-    ### zcat Homo_sapiens.GRCh38.105.gtf.gz | awk '$3 ~ /exon/' > Homo_sapiens.GRCh38.105_exons.gtf
-    def exon_pos(self):
-        dict_exon_pos = {}
-        with open(self.file, "r") as f:
-            for line in f.readlines():
-                line = line.strip()
-                exon_id = re.search(r'(?<=exon_id ")[^"\s]*',line)
-                line = line.split()
-                chrom = line[0]
-                start = line[3]
-                end = line[4]
-                if chrom not in dict_exon_pos.keys():
-                    dict_exon_pos[chrom] = {}
-                    dict_exon_pos[chrom][exon_id.group()] = start, end
-                else:
-                    dict_exon_pos[chrom][exon_id.group()] = start,end
-        return dict_exon_pos
-# test
-# exon_findings = Exon("exons_sample.gtf")
 
 class Sequence:
     '''This class represents all the sequences in a FASTA file.'''
@@ -110,7 +83,7 @@ class Sequence:
         ## Data ##
         self.file = file
         self.motif = Motif('Fig_1_motifs.txt')
-        self.exon = Exon("Homo_sapiens.GRCh38.105_exons.gtf")
+        #self.exon = Exon("Homo_sapiens.GRCh38.105_exons.gtf")
 
     ### Methods ##
     def extract_sequence(self):
@@ -128,9 +101,24 @@ class Sequence:
                     header = line.strip()
                     #print(header)
                 else:
-                    dict_seq[header] = line.strip()
+                    dict_seq[header] = line.strip().lower()
                     count += 1
         return dict_seq
+    def dictionary_sequence_upper(self):
+        ''''''
+        self.extract_sequence()
+        with open("oneline_fasta.txt", "r") as f:
+            count = 0
+            dict_seq_upper = {}
+            header= ""
+            for line in f.readlines():
+                if line[0] == ">":
+                    header = line.strip()
+                    #print(header)
+                else:
+                    dict_seq_upper[header] = line.strip()
+                    count += 1
+        return dict_seq_upper 
     def seq_length(self):
         dict_seq = self.dictionary_sequence()
         dict_length = {}
@@ -149,20 +137,61 @@ class Sequence:
                     dict_motif_in_seq[header][match.start()] = key
                     #print(key, match.start(), match.end())
         return dict_motif_in_seq
-    def find_exon(self):
-        dict_exon_pos = self.exon.exon_pos()
-        dict_length = self.seq_length()
-        for header,length in dict_length.items():
-            chrom = re.search(r'(?<=chr")[^:\s]*',header)
+    def sort_dict_motif_in_seq(self):
+        dict_motif_in_seq = self.find_motif()
+        dict_sorted_motif_in_seq = {header: {} for header,motif in dict_motif_in_seq.items()}
+        for header,motif in dict_motif_in_seq.items():
+            dict_sorted_motif_in_seq[header] = dict(sorted(dict_motif_in_seq[header].items()))
+        return dict_sorted_motif_in_seq
+    # def find_exon(self):
+    #     dict_exon_pos = self.exon.exon_pos()
+    #     dict_length = self.seq_length()
+    #     for header,length in dict_length.items():
+    #         chrom = re.search(r'(?<=chr")[^:\s]*',header)
             
-        print(header)
+    #     print(header)
 
-        
+    
 
 # sequence_findings = Sequence('Figure_1.fasta')
 # sequence_findings.extract_sequence()
 # sequence_findings.dictionary_sequence()
 # sequence_findings.find_motif()
+# sequence_findings.sort_dict_motif_in_seq()
+
+
+class Exon:
+    ''''''
+    def __init__(self):
+        '''DOCUMENT'''
+        ## Data ##
+        self.sequence = Sequence("Figure_1.fasta")
+    
+    ### Methods ###
+    ### zcat Homo_sapiens.GRCh38.105.gtf.gz | awk '$3 ~ /exon/' > Homo_sapiens.GRCh38.105_exons.gtf
+    def exon_pos(self):
+        dict_seq_upper = self.sequence.dictionary_sequence_upper()
+        #dict_exon_pos = {}
+        dict_exon_pos = {header: {} for header,seq in dict_seq_upper.items()}
+
+        #print(dict_seq)
+        for header,seq in dict_seq_upper.items():
+            count = 0
+            
+            for match in re.finditer(r'[A-Z]+',seq):
+                #print(header, match.group(), match.start(), match.end())
+                if header in dict_exon_pos.keys():
+                    dict_exon_pos[header][count] = [match.start(), match.end()]
+                    count += 1
+                else:
+                    continue
+        #print(dict_exon_pos)
+        return dict_exon_pos
+           
+
+# practice_exon = Exon()
+# practice_exon.exon_pos()
+
 
 class Visualization:
     ''''''
@@ -171,6 +200,7 @@ class Visualization:
         ## Data ##
         self.sequence = Sequence('Figure_1.fasta')
         self.motif = Motif('Fig_1_motifs.txt')
+        self.exon = Exon()
 
     ### Methods ##
     #def show_img(self):
@@ -182,7 +212,7 @@ class Visualization:
     def motif_color(self):
         dict_possible_motifs = self.motif.y_converter()
         dict_motif_color = {}
-        list_color_temp = [[193, 187, 221], [255,189,230],[252, 252, 148],[166,205,229]] #purple, pink, yellow, blue
+        list_color_temp = [[193, 187, 221], [255,189,230],[255, 215, 0],[166,205,229]] #purple, pink, yellow, blue
         list_color_temp_01 = []
         for col in list_color_temp:
             list_color_temp_01.append([x/255 for x in col])
@@ -195,8 +225,9 @@ class Visualization:
     def figure(self):
         dict_seq = self.sequence.dictionary_sequence()
         dict_length = self.sequence.seq_length()
-        dict_motif_in_seq = self.sequence.find_motif()
+        dict_sorted_motif_in_seq = self.sequence.sort_dict_motif_in_seq()
         dict_motif_color = self.motif_color()
+        dict_exon_pos = self.exon.exon_pos()
         max_length = dict_length[max(dict_length, key=dict_length.get)]
         #print(max_length)
         width = max_length + 50 + 300 #50 is for buffer? 300 is for legend space (250 legend, 50 buffer)
@@ -242,6 +273,7 @@ class Visualization:
             count +=1
 
         count=0
+        overlap = 0
         for header,length in dict_length.items():
             #header
             context.set_source_rgb(0,0,0)
@@ -257,22 +289,47 @@ class Visualization:
             context.set_source_rgb(0,0,0)
             context.stroke()
 
-            #motifs (rectangle)
-            ##### use the dictionary from Sequence. based on dictionary name, plot on sequence. if value is certain motif, use certain color and create rect.
-            for h,pairs in dict_motif_in_seq.items():
-                #print(h, pairs)
-
+            #exons (rectangle)
+            for h,exon_pos in dict_exon_pos.items():
                 if h == header:
-                    for start_pos,motif in pairs.items():
-                        context.set_source_rgb(dict_motif_color.get(motif)[0],dict_motif_color.get(motif)[1],dict_motif_color.get(motif)[2])
-                        context.rectangle(25+start_pos,100+(count*300),len(motif),100)
+                    for i,pos in exon_pos.items():
+                        start = pos[0]
+                        end = pos[1]-pos[0]
+                        context.set_source_rgb(0,0,0)
+                        context.rectangle(25+start,125+(count*300),end,50)
                         context.fill()
                 else:
                     continue
             
-            #exons (rectangle)
-            #context.rectangle(25+10,25-10,75-10-35,10+10)
-            #context.fill()
+            #motifs (rectangle)
+            ##### use the dictionary from Sequence. based on dictionary name, plot on sequence. if value is certain motif, use certain color and create rect.
+            for h,pairs in dict_sorted_motif_in_seq.items():
+                
+                #print(h, pairs)
+                overlap = 0
+                if h == header:
+                    for start_pos,motif in pairs.items():
+                        #print(start_pos)
+                            
+                            #overlap += 1
+                        if overlap % 2 == 0:
+                            context.set_source_rgb(dict_motif_color.get(motif)[0],dict_motif_color.get(motif)[1],dict_motif_color.get(motif)[2])
+                            
+                            context.rectangle(25+start_pos,120+(count*300),len(motif),25)
+                            context.fill()
+                            overlap += 1
+                            
+                        elif overlap % 2 == 1:
+                            context.set_source_rgb(dict_motif_color.get(motif)[0],dict_motif_color.get(motif)[1],dict_motif_color.get(motif)[2])
+
+                            context.rectangle(25+start_pos,95+(count*300),len(motif),25)
+                            context.fill()
+                            overlap += 1
+
+                else:
+                    continue
+                
+
 
             count += 1
 
